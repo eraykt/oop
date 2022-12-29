1535f5b2f9b699c3cd734ccdeb8bd2462fed727e
eray <alieraykt@gmail.com>
2022-12-29 11:12:56 +0300

    week12

--- labs/week11/BankAccount.java
+++ labs/week11/BankAccount.java
@@ -1,9 +1,11 @@
 package labs.week11;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
-public class BankAccount {
+public class BankAccount implements Comparable<BankAccount> {
     int accountNo;
     String holderName;
     double balance;
@@ -23,15 +25,23 @@ public class BankAccount {
         return accountNo + " " + holderName + " " + balance;
     }
 
+    @Override
+    public int compareTo(BankAccount o) {
+        return accountNo - o.accountNo;
+    }
+
     public static void main(String[] args) {
+        BankAccount a3 = new BankAccount(3, "semih", 1500);
         BankAccount a1 = new BankAccount(1, "eray", 1000);
         BankAccount a2 = new BankAccount(2, "tolga", 500);
-        BankAccount a3 = new BankAccount(3, "semih", 1500);
 
         ArrayList<BankAccount> myArrayList = new ArrayList<BankAccount>();
         myArrayList.add(a1);
         myArrayList.add(a2);
         myArrayList.add(a3);
+
+        Collections.sort(myArrayList, Collections.reverseOrder(new BalanceComp()));
+        System.out.println(myArrayList);
 
         Iterator<BankAccount> iter = myArrayList.iterator();
 
@@ -40,7 +50,21 @@ public class BankAccount {
             a.balanceChange(500);
             System.out.println(a);
         }
-
     }
+}
 
+class StringComp implements Comparator<BankAccount> {
+
+    @Override
+    public int compare(BankAccount o1, BankAccount o2) {
+        return o1.holderName.compareTo(o2.holderName);
+    }
+}
+
+class BalanceComp implements Comparator<BankAccount> {
+
+    @Override
+    public int compare(BankAccount o1, BankAccount o2) {
+        return (int) (o1.balance - o2.balance);
+    }
 }
